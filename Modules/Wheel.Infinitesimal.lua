0de8a75017222c963583242eecf08afbc205ef6c
dj505 <jaydenb505@gmail.com>
2022-03-11 16:28:31 -0700

    Begin work on wheel movement
    it difficult

--- Modules/Wheel.Infinitesimal.lua
+++ Modules/Wheel.Infinitesimal.lua
@@ -2,7 +2,62 @@ if not CurSong then CurSong = 1 end
 
 if not Joined then Joined = {} end
 
-local Offset = 7
+local IncOffset = 1  -- Positive rotation
+local DecOffset = 13 -- Negative rotation
+local XOffset = 7    -- The center of the wheel
+
+local function MoveSelection(self, offset, Songs)
+
+  CurSong = CurSong + offset
+
+  -- If selection is past the end of the list, wrap back to the first one
+	if CurSong > #Songs then CurSong = 1 end
+	-- If the selection is lower than one, wrap to the end of the list
+	if CurSong < 1 then CurSong = #Songs end
+
+  -- Update the offsets for increase/decrease
+	DecOffset = DecOffset + offset
+	IncOffset = IncOffset + offset
+
+  if DecOffset > 13 then DecOffset = 1 end
+	if IncOffset > 13 then IncOffset = 1 end
+
+  if DecOffset < 1 then DecOffset = 13 end
+  if IncOffset < 1 then IncOffset = 13 end
+
+  -- Update the offset for the center of the wheel
+	XOffset = XOffset + offset
+	if XOffset > 13 then XOffset = 1 end
+	if XOffset < 1 then XOffset = 13 end
+
+  -- If we're updating the offset with a value that isn't 0, we move the wheel
+  if offset ~= 0 then
+
+    for i = 1,13 do
+      local pos = CurSong + (4 * offset)
+
+      if offset == 1 then
+        pos = CurSong + (5 * offset)
+      end
+
+      while pos > #Songs do pos = pos-#Songs end
+			while pos < 1 do pos = #Songs+pos end
+
+      -- wheeeeeeeeee
+      self:GetChild("Wheel"):GetChild("Container"..i):decelerate(.1)
+      :addx(offset * -240)
+      :rotationy(clamp((i - XOffset) * 36, -85, 85))
+      :zoom(clamp(1.1 - (math.abs(i - XOffset) / 3), 0.8, 1.1))
+      :z(-math.abs(i - XOffset))
+
+      if (i == IncOffset and offset == -1) or (i == DecOffset and offset == 1) then
+        self:GetChild("Wheel"):GetChild("Container"..i):sleep(0):addx((offset*-240)*-13)
+      end
+    end
+
+  end
+
+end
 
 -- The wheel itself
 return function(Style)
@@ -35,8 +90,12 @@ return function(Style)
       end,
 
       Def.Banner {
-        -- This doesn't work and I don't understand why
-    		OnCommand=function(self)
+
+        InitCommand=function(self)
+          self:queuecommand("UpdateBanner")
+        end,
+
+    		UpdateBannerCommand=function(self)
           Song = Songs[i][1]
   				local Path = Song:GetBannerPath()
   				if not Path then
@@ -72,6 +131,21 @@ return function(Style)
   return Def.ActorFrame {
     OnCommand=function(self)
       self:Center():zoom(SCREEN_HEIGHT/720)
+
+      SCREENMAN:GetTopScreen():AddInputCallback(TF_WHEEL.Input(self))
+      MoveSelection(self,0,Songs)
+    end,
+
+    MenuLeftCommand=function(self)
+			MoveSelection(self,-1,Songs)
+		end,
+
+    MenuRightCommand=function(self)
+			MoveSelection(self,1,Songs)
+		end,
+
+    BackCommand=function(self)
+      SCREENMAN:GetTopScreen():SetNextScreenName(SCREENMAN:GetTopScreen():GetPrevScreenName()):StartTransitioningScreen("SM_GoToNextScreen")
     end,
 
     Wheel
