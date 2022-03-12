b87e7020ce9641b209003d8aa2393ab2e304ce16
dj505 <jaydenb505@gmail.com>
2022-03-11 20:11:18 -0700

    Wheel progress

--- Modules/Wheel.Infinitesimal.lua
+++ Modules/Wheel.Infinitesimal.lua
@@ -2,12 +2,15 @@ if not CurSong then CurSong = 1 end
 
 if not Joined then Joined = {} end
 
-local IncOffset = 1  -- Positive rotation
-local DecOffset = 13 -- Negative rotation
-local XOffset = 7    -- The center of the wheel
+local XOffset = 7 -- The center of the wheel
+local FirstOffset = 1 -- The leftmost item in the wheel
+local LastOffset = 13 -- The rightmost item in the wheel
+local Spacing = math.abs(math.sin(XOffset / math.pi))
 
 local function MoveSelection(self, offset, Songs)
 
+  -- "offset" should always be either -1 or 1, not to be confused with XOffset
+  -- Add 1 to go to the next song, add -1 to go to the previous song
   CurSong = CurSong + offset
 
   -- If selection is past the end of the list, wrap back to the first one
@@ -15,15 +18,15 @@ local function MoveSelection(self, offset, Songs)
 	-- If the selection is lower than one, wrap to the end of the list
 	if CurSong < 1 then CurSong = #Songs end
 
-  -- Update the offsets for increase/decrease
-	DecOffset = DecOffset + offset
-	IncOffset = IncOffset + offset
+  -- Update offsets for leftmost/rightmost values
+  FirstOffset = FirstOffset + offset
+  LastOffset = LastOffset + offset
 
-  if DecOffset > 13 then DecOffset = 1 end
-	if IncOffset > 13 then IncOffset = 1 end
+  if FirstOffset > 13 then FirstOffset = 1 end
+  if LastOffset > 13 then LastOffset = 1 end
 
-  if DecOffset < 1 then DecOffset = 13 end
-  if IncOffset < 1 then IncOffset = 13 end
+  if FirstOffset < 1 then FirstOffset = 13 end
+  if LastOffset < 1 then LastOffset = 13 end
 
   -- Update the offset for the center of the wheel
 	XOffset = XOffset + offset
@@ -34,26 +37,28 @@ local function MoveSelection(self, offset, Songs)
   if offset ~= 0 then
 
     for i = 1,13 do
-      local pos = CurSong + (4 * offset)
+      local pos = CurSong + (6 * offset)
 
       if offset == 1 then
-        pos = CurSong + (5 * offset)
+        pos = CurSong + (7 * offset)
       end
 
       while pos > #Songs do pos = pos-#Songs end
 			while pos < 1 do pos = #Songs+pos end
 
-      -- wheeeeeeeeee
-      self:GetChild("Wheel"):GetChild("Container"..i):decelerate(.1)
-      :addx(offset * -240)
+      -- Make the thing move
+      self:GetChild("Wheel"):GetChild("Container"..i)
+      :decelerate(.1)
+      :addx(-offset * (250 - Spacing * 100))
       :rotationy(clamp((i - XOffset) * 36, -85, 85))
       :zoom(clamp(1.1 - (math.abs(i - XOffset) / 3), 0.8, 1.1))
       :z(-math.abs(i - XOffset))
 
-      if (i == IncOffset and offset == -1) or (i == DecOffset and offset == 1) then
-        self:GetChild("Wheel"):GetChild("Container"..i):sleep(0):addx((offset*-240)*-13)
-        :GetChild("Banner"):queuecommand("UpdateBanner")
+      if (i == FirstOffset and offset == -1) or (i == LastOffset and offset == 1) then
+        self:GetChild("Wheel"):GetChild("Container"..i):sleep(0)
+        :addx((-offset * (250 - Spacing * 100))*-13)
       end
+
     end
 
   end
@@ -66,7 +71,7 @@ local function MoveSelection(self, offset, Songs)
 		if type(Songs[CurSong]) ~= "string" then
 			-- Play Current selected Song Music.
 			if Songs[CurSong][1]:GetMusicPath() then
-				SOUND:PlayMusicPart(Songs[CurSong][1]:GetMusicPath(),Songs[CurSong][1]:GetSampleStart(),Songs[CurSong][1]:GetSampleLength(),0,0,true)
+				SOUND:PlayMusicPart(Songs[CurSong][1]:GetMusicPath(),Songs[CurSong][1]:GetSampleStart(),Songs[CurSong][1]:GetSampleLength(),0,0,false)
 			end
 		end
 	end
@@ -78,6 +83,7 @@ return function(Style)
 
   -- Load all the songs for a specified style (currently just pump_single)
   local Songs = LoadModule("Songs.Loader.lua")(Style)
+
   -- Defining the wheel itself
   local Wheel = Def.ActorFrame{Name = "Wheel"}
 
@@ -94,19 +100,20 @@ return function(Style)
     Wheel[#Wheel+1] = Def.ActorFrame {
       Name="Container"..i,
       FOV=90,
+
       OnCommand=function(self)
-        local Spacing = math.abs(math.sin(Offset / math.pi))
-        self:x(Offset * (250 - Spacing * 100))
-        self:rotationy(clamp(Offset * 36, -85, 85))
-        self:z(-math.abs(Offset))
-        self:zoom(clamp(1.1 - (math.abs(Offset) / 3), 0.8, 1.1))
         self:ztest(true):zbuffer(true)
+
+        self:x(Offset * (250 - Spacing * 100))
+        :rotationy(clamp(Offset * 36, -85, 85))
+        :z(-math.abs(Offset))
+        :zoom(clamp(1.1 - (math.abs(Offset) / 3), 0.8, 1.1))
       end,
 
       Def.Banner {
         Name="Banner",
 
-        InitCommand=function(self)
+        OnCommand=function(self)
           self:queuecommand("UpdateBanner")
         end,
 
